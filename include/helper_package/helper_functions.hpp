81af02100bca149c4e963f8f8a6e910f6020edde
Kailash Khadka <k.khadka343@gmail.com>
2024-12-10 22:00:51 -0500

    Initial files for simple helper function

---
+++ include/helper_package/helper_functions.hpp
@@ -0,0 +1,13 @@
+#ifndef HELPER_FUNCTIONS_HPP
+#define HELPER_FUNCTIONS_HPP
+
+#include <string>
+
+namespace helper_package {
+
+// Example helper function
+std::string format_message(const std::string &message);
+
+}  // namespace helper_package
+
+#endif  // HELPER_FUNCTIONS_HPP
---
+++ src/helper_functions.cpp
@@ -0,0 +1,9 @@
+#include "helper_package/helper_functions.hpp"
+
+namespace helper_package {
+
+std::string format_message(const std::string &message) {
+    return "[Formatted]: " + message;
+}
+
+}  // namespace helper_package
