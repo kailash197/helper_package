652058e1f5ec7b5dddc64c395e5bbaba9be7c6a3
Kailash Khadka <k.khadka343@gmail.com>
2024-12-10 23:49:37 -0500

    Added degree conversions, find index functions

--- include/helper_package/helper_functions.hpp
+++ include/helper_package/helper_functions.hpp
@@ -2,11 +2,33 @@
 #define HELPER_FUNCTIONS_HPP
 
 #include <string>
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 namespace helper_package {
 
+using namespace rclcpp;
+using namespace std;
+using namespace std::chrono_literals;
+
+using LaserScan = sensor_msgs::msg::LaserScan;
+using Twist = geometry_msgs::msg::Twist;
+using Odometry = nav_msgs::msg::Odometry;
+
 // Example helper function
 std::string format_message(const std::string &message);
+
+// Function to find index for a given angle and specific points
+int find_index_scan_msg(const LaserScan::SharedPtr scan_msg, double angle=0.0);
+
+// Function to normalize angle to [-PI, PI]
+double normalize_angle(double angle);
+
+// Functions to convert degrees to radians and vice versa
+double rad2deg(double rad);
+double deg2rad(double deg);
 
 }  // namespace helper_package
 
--- src/helper_functions.cpp
+++ src/helper_functions.cpp
@@ -6,4 +6,37 @@ std::string format_message(const std::string &message) {
     return "[Formatted]: " + message;
 }
 
+// Function to find index for a given angle and specific points
+int find_index_scan_msg(const LaserScan::SharedPtr scan_msg, double angle) {
+    if (!scan_msg) {
+        RCLCPP_ERROR(rclcpp::get_logger("find_index_scan_msg"), "Invalid LaserScan message.");
+        return -1;
+    }
+
+    if (angle < scan_msg->angle_min || angle > scan_msg->angle_max) {
+        std::ostringstream error_msg;
+        error_msg << "Angle out of bounds, [" << scan_msg->angle_min << " ," << scan_msg->angle_max << "]";
+        throw std::invalid_argument(error_msg.str());
+    }
+
+    auto angle_to_index = [&](double ang) -> int {
+        return static_cast<int>((ang - scan_msg->angle_min) / scan_msg->angle_increment);
+    };
+
+    int temp = angle_to_index(angle);
+    RCLCPP_DEBUG(rclcpp::get_logger("find_index_scan_msg"), "Angle: %f, Index: %d", angle, temp);
+    return temp;
+}
+
+// Function to normalize angle to [-PI, PI]
+double normalize_angle(double angle) {
+    while (angle > M_PI) angle -= 2.0 * M_PI;
+    while (angle < -M_PI) angle += 2.0 * M_PI;
+  return angle;
+}
+
+// Functions to convert degrees to radians and vice versa
+double rad2deg(double rad) { return rad * 180.0 / M_PI; }
+double deg2rad(double deg) { return deg * M_PI / 180.0; }
+
 }  // namespace helper_package
